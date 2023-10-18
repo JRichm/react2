4268e20bfae38c418b5ce7fc3d42fe87c313347f
James Richmond <jblackenmusic@gmail.com>
2023-10-18 13:24:31 -0600

    hid scrollbar

--- src/app/components/asidePanel.tsx
+++ src/app/components/asidePanel.tsx
@@ -5,8 +5,8 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 export default function AsidePanel() {
     return (
         <>
-            <div className="border-r border-b border-black border-solid p-2 h-full">
-                <Image className="bg-black m-3" alt='test image not found' src="" width={150} height={150}></Image>
+            <div className="border-r border-b border-black border-solid p-2 h-full w-[500px]">
+                <Image className="bg-black m-3" alt='test image not found' src="" width={100} height={100}></Image>
                 <h1>test header for image</h1>
                 <hr />
                 <span>
--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,15 @@
+import MainHeader from "../components/mainHeader"
+
 export default function NewPostPage() {
     return (
         <>
-            <h1>header</h1>
+          <div className='flex flex-row w-full justify-center'>
+            <div className='w-[900px]'>
+              <MainHeader />
+              <div className='flex flex-row justify-center'>
+              </div>
+            </div>
+          </div>
         </>
     )
 }
