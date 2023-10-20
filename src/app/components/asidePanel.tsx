83fe7bebf1044b58952fe8c04ba272492e14c605
James Richmond <jblackenmusic@gmail.com>
2023-10-19 19:51:53 -0600

    got closer to spotify api authorization

--- src/app/components/asidePanel.tsx
+++ src/app/components/asidePanel.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const config = require('../../../config.json');
 const client_id = config.CLIENT_ID;
@@ -18,10 +18,10 @@ interface SpotifyApiResponse {
 
 export default function AsidePanel() {
 
+    const router = useRouter();
+
     const [displayedSong, setDisplayedSong] = useState<SpotifyApiResponse | undefined>();
     const [apiToken, setApiToken] = useState<string | undefined>();
-
-    const router = useRouter();
 
     // use effect
         // get api token
@@ -29,90 +29,113 @@ export default function AsidePanel() {
         // get displayed song
 
     useEffect(() => {
-        const fetchData = async () => {
+        const setToken = async () => {
 
             // get token for app only if its not already set
             if (!apiToken) {
-                const token = await get_token();
-                setApiToken(token);
+                const token = await get_token("")
+                setApiToken(token)
             }
 
-            if (!apiToken) {
-                console.error('(!) Error getting access token:', apiToken);
-                return;
-            }
             console.log('(*) Token:', apiToken);
 
             const url = new URL(window.location.href);
             const codeValue = url.searchParams.get('code');
-                    
-            // Authorize token
+            
+            //
             if (codeValue !== null) {
                 console.log('(*) Value of code param:', codeValue);
-            
+
+                const authorizedToken = await get_token(codeValue)
+                setApiToken(authorizedToken)
+
+            // Authorize token
+            } else {
+                console.log('(>) code param not found in url, authorizing')
                 await authorizeApp();
-    
+            }
+        }
+        setToken();
+    }, [displayedSong]);
+
+    useEffect(() => {
+        async function fetchData() {
+            if (apiToken) {
                 // Get displayed song
                 const song = await GetMostRecentSong();
                 console.log('(*) Song:', song);
-            } else {
-                console.log('(>) code param not found in url, authorizing')
             }
         }
         fetchData();
-    }, [apiToken]);
+    }, [apiToken])
 
     async function GetMostRecentSong() {
-        console.log('getting most recent song')
-        try {
-            console.log('(>) Getting Most Recent Song...')
+        console.log('(>) Getting Most Recent Song...')
+        console.log(apiToken)
     
-            if (!apiToken || apiToken == "") {
-                console.error('(!)Error getting access token:', apiToken)
-                return;
-            }
-    
+        if (!apiToken || apiToken == "") {
+            console.error('(!)Error getting access token:', apiToken);
+        } else {
             console.log('(✓) valid token')
-        
-            const apiResponse = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=1', {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${apiToken}`
-                },
-            });
-        
-            if (!apiResponse.ok) {
-                console.error('(!) Error fetching data from the Spotify API:', apiResponse.status, apiResponse.statusText);
-                return;
-            }
-        
-            const data = await apiResponse.json();
-        
-            console.log('data');
-            console.log(data);
-        
-            const mostRecentSong = data.items[0];
-            return mostRecentSong;
-        } catch (err) {
-            console.error('(!) Error fetching recently played song:', err);
         }
+
+        const apiResponse = await fetch(`https://api.spotify.com/v1/playlists/1okbgtASYcBbmjfaGA1jps&access_token=${apiToken}`, {
+            method: 'GET',
+        })
+
+        if (!apiResponse.ok) {
+            if (apiResponse.status == 401) {
+                const currentURL = new URL(window.location.href)
+                const code = "" + currentURL.searchParams.get('code')
+                const newToken = await get_token(code);
+                console.log('newToken')
+                console.log(newToken)
+                console.log(apiToken)
+
+                const authorizedToken = await get_token(newToken);
+                console.log('newAuthorizedToken')
+                console.log(authorizedToken)
+            }
+            console.error('(!) Error fetching data from the Spotify API:', apiResponse.status, apiResponse.statusText);
+            console.error(apiResponse);
+            return;
+        }
+
+        const data = await apiResponse.json();
+        
+        console.log('data');
+        console.log(data);
+    
+        const mostRecentSong = data.items[0];
+        return mostRecentSong;
     }
 
-    async function get_token() {
+    async function get_token(code: string) {
 
         // concat authString and convert to base64
         const authString = client_id + ":" + client_secret;
         const authBase64 = Buffer.from(authString, "utf-8").toString("base64");
-    
+        
+        var body = ""
+
+        if (code) {
+            body = 'grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret + '&code=' + code
+        } else {
+            body = 'grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret
+        }
+
         const token = await fetch('https://accounts.spotify.com/api/token', {
             method: 'POST',
             headers: {
                 'Authorization': `Basic ${authBase64}`,
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
-            body: 'grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret
+            body: body
         }).then(res => res.json())
         .then(async(data) => {
+            console.log('return valaue of get_token') 
+            console.log(data.access_token) 
+            console.log(code) 
             return data.access_token;
         })
         .catch(err => {
@@ -124,10 +147,8 @@ export default function AsidePanel() {
     async function authorizeApp() {
 
         const REDIRECT_URI = 'http://localhost:3000/';
-        const SCOPE = 'streaming user-read-email user-read-private user-library-read user-library-modify user-read-playback-state user-modify-playback-state'
-
+        const SCOPE = 'user-top-read'
         const state = generateRandomString(16);
-
         const authorizationURL = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${SCOPE}&state=${state}&redirect_uri=${REDIRECT_URI}`;
 
         try {
@@ -152,7 +173,7 @@ export default function AsidePanel() {
 
     return (
         <>
-            <div className="border-r border-b border-black border-solid p-2 h-full w-[2000px] flex flex-col">
+            <div className="border-r border-b border-black border-solid p-2 h-full w-[250px] flex flex-col">
                 <div className='flex flex-col'>
                     <div className='flex w-full justify-center'>
                         {displayedSong && displayedSong.album && (
@@ -169,7 +190,7 @@ export default function AsidePanel() {
                 </div>
                 <hr className='m-3' />
                 <span>
-                    <p>some other random jazz</p>
+                    <p>{apiToken}</p>
                 </span>
             </div>
         </>
